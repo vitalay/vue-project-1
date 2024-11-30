d3b424d8a35ff6cf9174bfdecc3675234132116f
vitalya.efimov <vitalay.efimov@gmail.com>
2024-11-30 21:52:06 +0500

    сделал API на погоду

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,76 @@
-
-
-<script >
+<script>
+import axios from 'axios'
 
 export default {
 
   data() {
     return {
-      city: ""
+      city: "",
+      error: "",
+      info: null
     }
   },
   computed: {
     cityName() {
       return "<<" + this.city + ">>"
+    },
+    showTemp() {
+      return "Температура: " + this.info.main.temp 
+    },
+    showFeelsLike() {
+      return "Ощущается как: " + this.info.main.feels_like
+    },
+    showMinTemp() {
+      return "Минимальная: " + this.info.main.temp_min
+    },
+    showMaxTemp() {
+      return "Максимальная: " + this.info.main.temp_max
     }
+    
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Название города должно быть больше 1 символов :)"
+        return false
+
+      }
+       this.error = ""
+
+       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=2ad553dea87107e590ed7de670b4a4d8`)   
+            .then(res => (this.info = res.data))
+    }
+
   }
+  
 
 }
-
 </script>
 
 <template>
-  
   <div class="wrapper">
-  <h1>Погодное приложение </h1>
-  <p>Узнать погоду в  {{ city ==  "" ? "вашем городе" : cityName }}</p>
-  <input type="text" v-model="city" placeholder="Введите город">
-  <button v-if="city != ''">Получить погоду</button>
-  <button disabled v-else>Введите название города</button>
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в {{ city == "" ? "вашем городе" : cityName }}</p>
+    <input type="text" v-model="city" placeholder="Введите город" />
+    <button v-if="city != ''" @click="getWeather()">Получить погоду</button>
+    <button disabled v-else>Введите название города</button>
+    <p class="error"> {{ error }} </p>
 
+    <div v-if="info != null">
+          <p>{{ showTemp }}</p>
+          <p>{{ showFeelsLike }}</p>
+          <p>{{ showMinTemp }}</p>
+          <p>{{ showMaxTemp }}</p>
+    </div>
+    
   </div>
-
 </template>
 
-
-
-
-
-
 <style scoped>
+.error {
+  color: #d03939;
+}
+
 .wrapper {
   width: 900px;
   height: 500px;
@@ -48,25 +81,23 @@ export default {
   color: #fff;
 }
 .wrapper h1 {
-margin-top: 50px;
-
+  margin-top: 50px;
 }
 .wrapper p {
-margin-top: 20px;
+  margin-top: 20px;
 }
 .wrapper input {
-margin-top: 35px;
-background: transparent;
-border: 0;
-border-bottom: 2px solid #110813;
-color: #fcfcfc;
-padding: 8px 15px;
-font-size: 18px;
-outline: none;
+  margin-top: 35px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  padding: 8px 15px;
+  font-size: 18px;
+  outline: none;
 }
 .wrapper input:focus {
-border-bottom: 2px solid #e912d7;
-
+  border-bottom: 2px solid #e912d7;
 }
 
 .wrapper button:disabled {
